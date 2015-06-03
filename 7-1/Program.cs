00b389bddcf4d1992b453a831e40b47f1afde233
MarthaLRigby <martha.l.rigby@gmail.com>
2015-06-03 01:26:58 +0100

    7-2

--- 7-1/Program.cs
+++ 7-1/Program.cs
@@ -12,29 +12,28 @@ namespace _7_1
         {
             int a = 6;
             int b = 2;
-            Math m = new Math();
             Console.WriteLine("a = {0}, b = {1}", a, b);
-            Console.WriteLine("Add(a,b) = {0}", m.add(a, b));
-            Console.WriteLine("Subtract(a,b) = {0}", m.subtract(a, b));
-            Console.WriteLine("Multiply(a,b) = {0}", m.multiply(a, b));
-            Console.WriteLine("Divide(a,b) = {0}", m.divide(a, b));
+            Console.WriteLine("Add(a,b) = {0}", Math.add(a, b));
+            Console.WriteLine("Subtract(a,b) = {0}", Math.subtract(a, b));
+            Console.WriteLine("Multiply(a,b) = {0}", Math.multiply(a, b));
+            Console.WriteLine("Divide(a,b) = {0}", Math.divide(a, b));
         }
     }
     class Math
     {
-        public int add(int a, int b)
+        public static int add(int a, int b)
         {
             return a + b;
         }
-        public int subtract(int a, int b)
+        public static int subtract(int a, int b)
         {
             return a - b;
         }
-        public int multiply(int a, int b)
+        public static int multiply(int a, int b)
         {
             return a * b;
         }
-        public int divide(int a, int b)
+        public static int divide(int a, int b)
         {
             return a / b;
         }
